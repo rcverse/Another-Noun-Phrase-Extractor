f54088b0e9d7dac24f308ef2d6b9f04012ce4bdc
Richard <richard20000321@gmail.com>
2025-04-17 10:02:30 +0800

    GUI migrated to new repo

--- anpe_gui/workers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Make worker classes available."""
-
-from .extraction_worker import ExtractionWorker
-from .batch_worker import BatchWorker
-from .log_handler import QtLogHandler # Keep existing if needed
-# Add others if they exist and are needed 
